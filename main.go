6d76d98f2a12b07b1af3d3fc56efb9aebe8cbcb9
afif.fatakhul <fatakhulafi11@gmail.com>
2022-11-27 11:01:40 +0700

    File

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"crypto/sha1"
 	"encoding/base64"
 	"fmt"
+	"io"
 	"math"
 	"math/rand"
+	"os"
 	"project-pertama/library"
 	"runtime"
 	"strconv"
@@ -371,6 +373,13 @@ outerLoop:
 	fmt.Printf("hashed 3 : %s\n\n", hashed3)
 
 	_, _, _ = salt1, salt2, salt3
+
+	// File
+	var path = "/home/afi/Documents/test.txt"
+	createFile(path)
+	writeFile(path)
+	readFile(path)
+	deleteFile(path)
 }
 
 // contoh fungsi
@@ -478,4 +487,94 @@ func doHashUsingSalt(text string) (string, string) {
 	var encrypted = sha.Sum(nil)
 
 	return fmt.Sprintf("%x", encrypted), salt
+}
+
+// fungsi file
+func isError(err error) bool {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return (err != nil)
+}
+
+func createFile(path string) {
+	// deteksi apakah file sudah ada
+	var _, err = os.Stat(path)
+
+	// buat file baru jika belum ada
+	if os.IsNotExist(err) {
+		var file, err = os.Create(path)
+		if isError(err) {
+			return
+		}
+		defer file.Close()
+	}
+
+	fmt.Println("==> file berhasil dibuat", path)
+}
+
+func writeFile(path string) {
+	// buka file dengan level akses READ & WRITE
+	var file, err = os.OpenFile(path, os.O_RDWR, 0644)
+	if isError(err) {
+		return
+	}
+	defer file.Close()
+
+	// tulis data ke file
+	_, err = file.WriteString("halo\n")
+	if isError(err) {
+		return
+	}
+	_, err = file.WriteString("mari belajar golang\n")
+	if isError(err) {
+		return
+	}
+
+	// simpan perubahan
+	err = file.Sync()
+	if isError(err) {
+		return
+	}
+
+	fmt.Println("==> file berhasil di isi")
+}
+
+func readFile(path string) {
+	// buka file
+	var file, err = os.OpenFile(path, os.O_RDONLY, 0644)
+	if isError(err) {
+		return
+	}
+	defer file.Close()
+
+	// baca file
+	var text = make([]byte, 1024)
+	for {
+		n, err := file.Read(text)
+		if err != io.EOF {
+			if isError(err) {
+				break
+			}
+		}
+		if n == 0 {
+			break
+		}
+	}
+	if isError(err) {
+		return
+	}
+
+	fmt.Println("==> file berhasil dibaca")
+	fmt.Println(string(text))
+}
+
+func deleteFile(path string) {
+	var err = os.Remove(path)
+	if isError(err) {
+		return
+	}
+
+	fmt.Println("==> file berhasil di delete")
 }
